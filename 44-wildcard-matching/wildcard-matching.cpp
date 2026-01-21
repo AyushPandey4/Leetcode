0316d7fab88fa51a3df56387323618ffa301b1c8
Ayush Pandey <121339885+AyushPandey4@users.noreply.github.com>
2026-01-21 15:35:43 +0530

    Time: 71 ms (20.43%) | Memory: 15.8 MB (53.93%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    bool solve(int i, int j, string& p, string& s, vector<vector<int>>& dp) {
-        if (i < 0 && j < 0)
-            return true;
-
-        if (i < 0 && j >= 0)
-            return false;
-
-        if (j < 0 && i >= 0) {
-            for (int k = 0; k <= i; k++)
-                if (p[k] != '*')
-                    return false;
-            return true;
+    bool isAllStars(string& p, int j) {
+        for (int k = 0; k < j; k++) {
+            if (p[k] != '*')
+                return false;
         }
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        if (p[i] == s[j] || p[i] == '?')
-            return dp[i][j] = solve(i - 1, j - 1, p, s, dp);
-
-        if (p[i] == '*')
-            return dp[i][j] = solve(i - 1, j, p, s, dp) || // '*' matches empty
-                              solve(i, j - 1, p, s, dp);   // '*' matches char
-
-        return dp[i][j] = false;
+        return true;
     }
 
     bool isMatch(string s, string p) {
-        int n = p.size(), m = s.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n - 1, m - 1, p, s, dp);
+        int n = s.length(); // string
+        int m = p.length(); // pattern
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+        dp[0][0] = true;
+
+        // empty string vs pattern
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = isAllStars(p, j);
+        }
+
+        // string vs empty pattern already false by default
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 1] || // '*' matches empty
+                               dp[i - 1][j];   // '*' matches one char
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
