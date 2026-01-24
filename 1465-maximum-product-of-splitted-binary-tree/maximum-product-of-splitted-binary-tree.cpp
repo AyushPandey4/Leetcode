468560aa828cdf722908a63a1db72511f2d48ce9
Ayush Pandey <121339885+AyushPandey4@users.noreply.github.com>
2026-01-24 13:53:54 +0530

    Time: 12 ms (15.23%) | Memory: 78 MB (80.75%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    long long totalSum = 0;
-    long long best = 0;
-    const int MOD = 1e9 + 7;
+    static const int MOD = 1e9 + 7;
+    long long total = 0;
+    long long ans = 0;
 
-    long long dfs(TreeNode* node) {
-        if (!node)
+    long long getTotal(TreeNode* root) {
+        if (!root)
+            return 0;
+        return root->val + getTotal(root->left) + getTotal(root->right);
+    }
+
+    long long dfs(TreeNode* root) {
+        if (!root)
             return 0;
 
-        long long left = dfs(node->left);
-        long long right = dfs(node->right);
+        long long left = dfs(root->left);
+        long long right = dfs(root->right);
 
-        long long subTreeSum = left + right + node->val;
+        long long subSum = root->val + left + right;
 
-        best = max(best, subTreeSum * (totalSum - subTreeSum));
+        ans = max(ans, subSum * (total - subSum));
 
-        return subTreeSum;
+        return subSum;
     }
 
     int maxProduct(TreeNode* root) {
-        dfsSum(root);
-
+        total = getTotal(root);
         dfs(root);
-
-        return best % MOD;
-    }
-
-private:
-    long long dfsSum(TreeNode* node) {
-        if (!node)
-            return 0;
-        totalSum += node->val;
-        dfsSum(node->left);
-        dfsSum(node->right);
-        return totalSum;
+        return ans % MOD;
     }
 };
