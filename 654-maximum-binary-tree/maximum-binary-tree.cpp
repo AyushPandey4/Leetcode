d3fc4debce9ab81b0327e78a3ee307b73913cdf1
Ayush Pandey <121339885+AyushPandey4@users.noreply.github.com>
2026-02-11 20:33:18 +0530

    Time: 2 ms (94.88%) | Memory: 40.8 MB (80.83%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    TreeNode* build(vector<int>& nums, int l, int r) {
-        if (l > r)
-            return nullptr;
-        int maxind = l;
-        for (int i = l; i <= r; i++) {
-            if (nums[i] > nums[maxind]) {
-                maxind = i;
-            }
-        }
-        TreeNode* root = new TreeNode(nums[maxind]);
-        root->left = build(nums, l, maxind - 1);
-        root->right = build(nums, maxind + 1, r);
-        return root;
-    }
-
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return build(nums, 0, nums.size() - 1);
+        stack<TreeNode*> st;
+
+        for (int num : nums) {
+            TreeNode* curr = new TreeNode(num);
+
+            while (!st.empty() && st.top()->val < num) {
+                curr->left = st.top();
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                st.top()->right = curr;
+            }
+
+            st.push(curr);
+        }
+
+        while (st.size() > 1)
+            st.pop();
+
+        return st.top();
     }
 };
