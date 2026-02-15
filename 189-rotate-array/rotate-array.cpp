99507adcd30a5038f24868884aa427ea34894e65
Ayush Pandey <121339885+AyushPandey4@users.noreply.github.com>
2026-02-15 13:24:42 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (43.81%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -5,16 +5,8 @@ public:
         k = k % n;
         if (k == n)
             return;
-        vector<int> temp(n, 0);
-        int j = 0;
-        for (int i = n - k; i < n; i++) {
-            temp[j] = nums[i];
-            j++;
-        }
-        for (int i = 0; i <= n - k - 1; i++) {
-            temp[j] = nums[i];
-            j++;
-        }
-        nums = temp;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
